6e8b81c5dd8673d1084aa2f4ea48971ec3842fe3
Alexnoch <44502017+Alexnoch@users.noreply.github.com>
2020-11-12 00:01:54 +0300

    sdfsd

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const http = require('http');
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host:'31.184.249.2',
+    user:'begeek',
+    password:'123',
+    database:'cooking_dish'
+});
+connection.connect();
+
 
 http.createServer((req,res)=>{
+
     res.writeHead(200,{
         "Access-Control-Allow-Origin":"*",
         "Content-Type": "application/json",
         "Access-Control-Allow-Credentials":"true"
         });
-    res.end('Hello World');
+    let query_last = `SELECT * FROM first_dish`;
+    connection.query(query_last,function(error,result){
+        if(error) throw error;
+        console.log('Сработал кейс "последний рецепт"',error)
+        result = JSON.stringify(result[0]);
+        res.end(result);
+    });
 }).listen(3000);
