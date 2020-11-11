8658041ce1f7ac0cddc8387f0944fbf520978f5e
Alexnoch <44502017+Alexnoch@users.noreply.github.com>
2020-11-11 21:36:14 +0300

    lasting

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const http = require('http');
 
 http.createServer((req,res)=>{
+    res.writeHead(200,{
+        "Access-Control-Allow-Origin":"*",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials":"true"
+        });
     res.end('Hello World');
 }).listen(80);
